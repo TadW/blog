251779d8ad63b02f7136375e1ccb182624fa954b
TadW <tadwiecz@gmail.com>
2018-03-21 13:39:54 +0100

    update

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 
 
 INSERT INTO post (post_id, author, title, body, posted_on)
-VALUES (1, 'JoeK', 'Lorem ipsum dolor sit amet ',
+VALUES (1, 'Admin', 'Lorem ipsum dolor sit amet ',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         CURRENT_TIMESTAMP);
 INSERT INTO post (post_id, author, title, body, posted_on)
-VALUES (2, 'PaulW', 'Lorem ipsum dolor sit amet ',
+VALUES (2, 'Admin', 'Lorem ipsum dolor sit amet ',
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
         CURRENT_TIMESTAMP);
-INSERT INTO post (post_id, author, title, body, posted_on) VALUES (3, 'JasonA', 'Lorem ipsum dolor sit amet ',
+INSERT INTO post (post_id, author, title, body, posted_on) VALUES (3, 'Admin', 'Lorem ipsum dolor sit amet ',
                                                                    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
                                                                    CURRENT_TIMESTAMP);
 
